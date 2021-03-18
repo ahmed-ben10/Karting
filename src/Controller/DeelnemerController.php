95b799e3cd689ec43684c9f798f261e7e37efc5d
Ahmed <ahmed@securelogin.nu>
2021-03-18 18:25:20 +0100

    ticket A7

--- src/Controller/DeelnemerController.php
+++ src/Controller/DeelnemerController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class DeelnemerController extends AbstractController
 {
@@ -13,25 +18,25 @@ class DeelnemerController extends AbstractController
      */
     public function activiteitenAction()
     {
-        $usr= $this->get('security.token_storage')->getToken()->getUser();
+        $usr = $this->get('security.token_storage')->getToken()->getUser();
 
-        $beschikbareActiviteiten=$this->getDoctrine()
+        $beschikbareActiviteiten = $this->getDoctrine()
             ->getRepository('App:Activiteit')
             ->getBeschikbareActiviteiten($usr->getId());
 
-        $ingeschrevenActiviteiten=$this->getDoctrine()
+        $ingeschrevenActiviteiten = $this->getDoctrine()
             ->getRepository('App:Activiteit')
             ->getIngeschrevenActiviteiten($usr->getId());
 
-        $totaal=$this->getDoctrine()
+        $totaal = $this->getDoctrine()
             ->getRepository('App:Activiteit')
             ->getTotaal($ingeschrevenActiviteiten);
 
 
         return $this->render('deelnemer/activiteiten.html.twig', [
-            'beschikbare_activiteiten'=>$beschikbareActiviteiten,
-            'ingeschreven_activiteiten'=>$ingeschrevenActiviteiten,
-            'totaal'=>$totaal,
+            'beschikbare_activiteiten' => $beschikbareActiviteiten,
+            'ingeschreven_activiteiten' => $ingeschrevenActiviteiten,
+            'totaal' => $totaal,
         ]);
     }
 
@@ -44,7 +49,7 @@ class DeelnemerController extends AbstractController
         $activiteit = $this->getDoctrine()
             ->getRepository('App:Activiteit')
             ->find($id);
-        $usr= $this->get('security.token_storage')->getToken()->getUser();
+        $usr = $this->get('security.token_storage')->getToken()->getUser();
         $usr->addActiviteit($activiteit);
 
         $em = $this->getDoctrine()->getManager();
@@ -62,11 +67,44 @@ class DeelnemerController extends AbstractController
         $activiteit = $this->getDoctrine()
             ->getRepository('App:Activiteit')
             ->find($id);
-        $usr= $this->get('security.token_storage')->getToken()->getUser();
+        $usr = $this->get('security.token_storage')->getToken()->getUser();
         $usr->removeActiviteit($activiteit);
         $em = $this->getDoctrine()->getManager();
         $em->persist($usr);
         $em->flush();
         return $this->redirectToRoute('activiteiten');
+    }
+
+    /**
+     * @Route("/user/deelnemer/edit", name="deelnemer_edit")
+     */
+    public function editSoortActiviteitenAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->add('save', SubmitType::class, array('label' => "Wijzig"));
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'Gebruiker gewijzigd!'
+            );
+            return $this->redirectToRoute('activiteiten');
+        }
+
+        return $this->render('deelnemer/wijzig_deelnemer.html.twig',
+            array('form' => $form->createView(),
+                'naam' => 'wijzigen',
+            )
+        );
     }
 }
